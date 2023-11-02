b627683b31460fc62148b2c86227605e3606d961
AU\EnrightJ <jamesenright43@gmail.com>
2023-11-02 22:00:24 +1100

    mk_analysis function to return result and stats of a mann_kendall test from the trend package. For use in future full dataset mk function

---
+++ R/mk_analysis.R
@@ -0,0 +1,20 @@
+#' Mann_Kendall Test returning test result and stats
+#'
+#' @param data filtered tibble with column of "concentration"
+#'
+#' @return tibble with result and stats
+#' @export
+#'
+#' @examples mk_analysis(df)
+#' @importFrom trend mk.test
+
+mk_analysis <- function(data) {
+  result <- trend::mk.test(data$concentration)
+  p_value <- result$p.value
+  tau_statistic <- result$estimates[3]
+  sample_mean <- base::mean(data$concentration, na.rm=T)
+  SD <- stats::sd(data$concentration, na.rm=T)
+  COV <-  SD / sample_mean
+  return(c(p_value, tau_statistic, sample_mean, SD, COV))
+
+}
