701231cf915bf5f74bac3b86c78638aa45cf0369
AU\EnrightJ <jamesenright43@gmail.com>
2023-11-12 19:20:05 +1100

    Add zones to global assignment

--- R/establish_plotting_variables.R
+++ R/establish_plotting_variables.R
@@ -11,31 +11,25 @@
 #' @importFrom Polychrome alphabet.colors
 
 establish_plotting_variables <- function(data){
-  chemgroup <- base::unique(data$group)
+  chemgroup <<- base::unique(data$group)
 
-  analytes <- dplyr::filter(data, group %in% chemgroup) %>%
+  analytes <<- dplyr::filter(data, group %in% chemgroup) %>%
     dplyr::select(analyte) %>%
     dplyr::distinct() %>%
     tidyr::drop_na() %>%
     base::as.vector() %>%
     base::unlist()
 
-  date_range <- c(base::min(data$date), base::max(data$date))
+  date_range <<- c(base::min(data$date), base::max(data$date))
 
-  zones <- base::unique(data$zone)
+  zones <<- base::unique(data$zone)
 
-  locations_vec <- base::unique(data$location)
+  locations_vec <<- base::unique(data$location)
 
-  colours_vec <- Polychrome::alphabet.colors(n = base::length(locations_vec))
+  colours_vec <<- Polychrome::alphabet.colors(n = base::length(locations_vec))
 
   # assign locations to colours
-  location_colours <- stats::setNames(colours_vec, locations_vec)
-
-
-  return(analytes)
-  return(zones)
-  return(locations_vec)
-  return(colours_vec)
+  location_colours <<- stats::setNames(colours_vec, locations_vec)
 
 }
 
