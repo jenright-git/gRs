485f8ffad27452e0aa678a1c7d2ea7571ee1e5d8
AU\EnrightJ <jamesenright43@gmail.com>
2024-02-28 15:08:48 +1100

    Update the "dissolved" function to remove 'analyte' and replace with 'chem_name'

--- R/data_processor.R
+++ R/data_processor.R
@@ -31,34 +31,13 @@ data_processor <- function(myfile_path){
                                                                               "text", "numeric", "numeric")
   )
 
-  sw_data <-
-    #   dplyr::select(raw_sw_data,
-    #                          lat = Latitude,
-    #                          lon = Longitude,
-    #                          site = Site_ID,
-    #                          location = Location_Code,
-    #                          field = Field_ID,
-    #                          sample_date = Sampled_Date_Time,
-    #                          monitoring_round = Monitoring_Round,
-    #                          zone = Monitoring_Zone,
-    #                          analyte = ChemName,
-    #                          filtered = Total_or_Filtered,
-  #                          prefix = Prefix,
-  #                          concentration = Concentration,
-  #                          group = Chem_Group,
-  #                          units = `Output Unit`,
-  #                          location_type = Location_Type,
-  #                          SampleComments,
-  #                          sample_type = Sample_Type,
-  #                          purpose = Purpose
-  # ) %>%
-  raw_sw_data %>%
+  sw_data <- raw_sw_data %>%
     janitor::clean_names() %>%
     dplyr::mutate(date = lubridate::floor_date(sampled_date_time, "day"))
 
   sw_data <-
     dplyr::mutate(sw_data,
-                  analyte = ifelse(total_or_filtered == "F",
+                  chem_name = ifelse(total_or_filtered == "F",
                                    yes = glue::glue("Dissolved {chem_name}"),
                                    no = chem_name)) %>%
     dplyr::arrange(date)
